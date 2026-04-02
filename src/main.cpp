cfbbcaa61baa8f2ef4327e89ff7420ece513be71
Kacang Tojin <amanfauzan31@gmail.com>
2026-04-02 23:33:12 +0700

    output txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
+#include <fstream>
 #include "lexer.hpp"
 #include "token.hpp"
 using namespace std;
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        cout << "Usage: " << argv[0] << " <source_file>" << endl;
+        cout << "Usage: " << argv[0] << " <source_file> [output_file]" << endl;
         return 1;
     }
     string filename = argv[1];
     Lexer lexer;
     lexer.DFA(filename);
     vector<Token> tokens = lexer.getTokens();
+
+    ostream* out = &cout;
+    ofstream outFile;
+    if (argc >= 3) {
+        outFile.open(argv[2]);
+        if (!outFile.is_open()) {
+            cout << "[!] Output file tidak bisa dibuka." << endl;
+            return 1;
+        }
+        out = &outFile;
+    }
+
     for (const Token& token : tokens) {
-        if (token.type == TokenType::IDENT || token.type == TokenType::INTCON || 
-            token.type == TokenType::REALCON || token.type == TokenType::CHARCON || 
+        if (token.type == TokenType::IDENT || token.type == TokenType::INTCON ||
+            token.type == TokenType::REALCON || token.type == TokenType::CHARCON ||
             token.type == TokenType::STRING || token.type == TokenType::ERROR ||
             token.type == TokenType::COMMENT) {
-            cout << tokenToString(token.type) << " (" << token.value << ")" << endl;
+            *out << tokenToString(token.type) << " (" << token.value << ")" << endl;
         } else {
-            cout << tokenToString(token.type) << endl;
+            *out << tokenToString(token.type) << endl;
         }
     }
     return 0;
